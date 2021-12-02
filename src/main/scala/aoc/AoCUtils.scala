142c2649d4bbbca23995023d9579245e5f408f6f
Julia Reynolds <julia.reynolds@cision.com>
2021-12-02 09:31:02 -0600

    complete day 2

--- src/main/scala/aoc/AoCUtils.scala
+++ src/main/scala/aoc/AoCUtils.scala
@@ -6,5 +6,8 @@ object AoCUtils {
   def parse_input(filePath: String): List[Int] =
     (for (x <- Source.fromFile(filePath).getLines()) yield x.toInt).toList
 
+  def parse_string_input(filePath: String): List[String] =
+    (for (x <- Source.fromFile(filePath).getLines()) yield x.toString).toList
+
 
 }
