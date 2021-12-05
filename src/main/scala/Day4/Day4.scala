3c74dec00fe05f8b0f73ad0cc6d46c81b3eadfb6
Julia Reynolds <julia.reynolds@cision.com>
2021-12-04 20:37:16 -0600

    day 4 complete

--- src/main/scala/Day4/Day4.scala
+++ src/main/scala/Day4/Day4.scala
@@ -1,152 +1,70 @@
 package Day4
 
-import aoc.AoCUtils.parse_string_input
+import aoc.AoCUtils.{parse_input_for_iterator, parse_string_input}
 
 import scala.annotation.tailrec
 import scala.collection.mutable
 
 object Day4 extends App {
 
-  val inputList = parse_string_input("/Users/julia.reynolds/Workspace/aoc-scala-2021/src/main/scala/Day3/input.txt")
+  val inputIterator = parse_input_for_iterator("/Users/julia.reynolds/Workspace/aoc-scala-2021/src/main/scala/Day4/input.txt")
 
-  var oneCount: Int = 0
-  var zeroCount: Int = 0
+  //get balls from input line 1
+  val balls = inputIterator.next().split(',').map(_.toInt)
+  //get cards from input line 2 ->
+  val cards = inputIterator.sliding(5,5).map(_.toArray).
+    map(_.map(row => row.trim.split("\\D+").map(value => (value.toInt, 0)))).
+    toArray
 
-  val indexBitCounterList = initPositionBitCounters()
+  @tailrec def calculateScore(balls: Array[Int], cards: Array[Array[Array[(Int, Int)]]], position: Int = 0): Int = {
+    val result = cards.
+      filter(block => !block.filter(row => row.foldLeft(0)(_ + _._2) == 5).isEmpty ||
+        !block.transpose.filter(row => row.foldLeft(0)(_ + _._2) == 5).isEmpty)
 
-  var gammaRate: String = ""
-  var epsilonRate: String = ""
-
-  def zeroCounters() = {
-    zeroCount = 0
-    oneCount = 0
-  }
-
-
-  def parseCode(index: Int, code: String): Boolean = {
-    if (index >= (code.length)) true
+    if(result.isEmpty){
+      val moddata = cards.map(block => {
+        block.map(row => {
+          row.map(value => {
+            if(value._1 == balls(position)) (value._1, 1) else value
+          })
+        })
+      })
+      calculateScore(balls, moddata, position + 1)
+    }
     else {
-      code.toCharArray.apply(index) match {
-        case '0' => indexBitCounterList.apply(index).incrementZero()
-        case '1' => indexBitCounterList.apply(index).incrementOne()
-      }
-      parseCode(index + 1, code)
+      result.
+        head.
+        reduce(Array.concat(_, _)).
+        foldLeft(0)( (acc, value) => if(value._2 == 0) acc + value._1 else acc ) * balls(position - 1)
     }
-
   }
+  @tailrec def calculateLoser(balls: Array[Int], cards: Array[Array[Array[(Int, Int)]]], position: Int = 0): Int = {
+    val uncompleteblocks = cards.
+      filter(block => block.filter(row => row.foldLeft(0)(_ + _._2) == 5).isEmpty &&
+        block.transpose.filter(row => row.foldLeft(0)(_ + _._2) == 5).isEmpty)
 
-  def countBitsAtIndexForList(characterIndex: Int, filterList: List[String]): BitCount = {
-    var index: Int = 0
-    val bitCount: BitCount = new BitCount(0, 0)
-    while (index < filterList.size) {
-      filterList.apply(index).toCharArray.apply(characterIndex) match {
-        case '0' => bitCount.incrementZero()
-        case '1' => bitCount.incrementOne()
-      }
-      index = index + 1
-    }
-    bitCount
-  }
-
-  def writeGammaAndEpsilonBits(): Boolean = {
-    var index: Int = 0
-    while (index < indexBitCounterList.size) {
-      if (indexBitCounterList.apply(index).zeroCount == indexBitCounterList.apply(index).oneCount) println("equal bit count cannot write value")
-      else if (indexBitCounterList.apply(index).zeroCount > indexBitCounterList.apply(index).oneCount) {
-        gammaRate = gammaRate + "0"
-        epsilonRate = epsilonRate + "1"
-      } else {
-        gammaRate = gammaRate + "1"
-        epsilonRate = epsilonRate + "0"
-      }
-      index = index + 1
-    }
-
-    true
-  }
-
-  @tailrec
-  def calculatePowerConsumption(index: Int): Boolean = {
-    if (index >= inputList.length) return true
+    if(uncompleteblocks.isEmpty && cards.size == 1)
+      cards.
+        head.
+        reduce(Array.concat(_, _)).
+        foldLeft(0)( (acc, value) => if(value._2 == 0) acc + value._1 else acc ) * balls(position - 1)
     else {
-      parseCode(0, inputList.apply(index))
+      val moddata = uncompleteblocks.map(block => {
+        block.map(row => {
+          row.map(value => {
+            if(value._1 == balls(position)) (value._1, 1) else value
+          })
+        })
+      })
+      calculateLoser(balls, moddata, position + 1)
     }
-    calculatePowerConsumption(index + 1)
   }
 
-  def initPositionBitCounters(): mutable.Map[Int, BitCount] = {
-    var bitCountMap = mutable.Map[Int, BitCount]()
-    var index: Int = 0
-    while (index < inputList.apply(0).length) {
-      bitCountMap(index) = new BitCount(0, 0)
-      index = index + 1
-    }
-    bitCountMap
-  }
+//read and match balls until a complete row or column is found
+  //sum all unmatched numbers on winning card
+  //multiply sum with last ball to get score
 
-
-  def filterForOxyGenRating(index: Int, filterList: List[String]): String = {
-    var filteredOxyGen: List[String] = List()
-    //already know the most popular in the starting list index 0
-    filteredOxyGen = filterList.filter(_.charAt(0) == indexBitCounterList.apply(0).mostCommonBit())
-    var localIndex = 1
-    if (filteredOxyGen.size == 1) println(filteredOxyGen.apply(0))
-    else {
-      while (localIndex < filterList.apply(0).length) {
-        val bitCount: BitCount = countBitsAtIndexForList(localIndex, filteredOxyGen)
-        filteredOxyGen = filteredOxyGen.filter(_.charAt(localIndex) == bitCount.mostCommonBit())
-        localIndex = localIndex + 1
-      }
-    }
-    filteredOxyGen.head
-  }
-
-  def filterForC02ScrubberRating(index: Int, filterList: List[String]): String = {
-    var filteredC02Scrubber: List[String] = List()
-    filteredC02Scrubber = filterList.filter(_.charAt(0) != indexBitCounterList.apply(0).mostCommonBit())
-    var localIndex = 1
-    if (filteredC02Scrubber.size == 1) println(filteredC02Scrubber.apply(0))
-    else {
-      while (localIndex < filterList.apply(0).length && filteredC02Scrubber.size>1) {
-        val bitCount: BitCount = countBitsAtIndexForList(localIndex, filteredC02Scrubber)
-        filteredC02Scrubber = filteredC02Scrubber.filter(_.charAt(localIndex) != bitCount.mostCommonBit())
-        localIndex = localIndex + 1
-      }
-    }
-    filteredC02Scrubber.head
-  }
-
-  initPositionBitCounters()
-  calculatePowerConsumption(0)
-  //writeGammaAndEpsilonBits()
-  //println("gammaRate: " + gammaRate)
-  //println("epsilonRate: " + epsilonRate)
-//  println("filterForOxyGenRating " + filterForOxyGenRating(0, inputList))
-//  println("filterForC02ScrubberRating " + filterForC02ScrubberRating(0, inputList))
-
-  // println("oxyGenList : " + oxyGenList)
-
-  //println("power consumption : " + (Integer.parseInt(gammaRate, 2) * Integer.parseInt(epsilonRate, 2)))
-  val oxy = Integer.parseInt(filterForOxyGenRating(0, inputList), 2)
-  val co2 = Integer.parseInt(filterForC02ScrubberRating(0, inputList), 2)
-  println("life support rating : " + (oxy * co2) )
-
+  println(calculateScore(balls,cards))
+  println(calculateLoser(balls,cards))
 }
 
-class BitCount(var zeroCount: Int, var oneCount: Int) {
-  def incrementOne(): Boolean = {
-    oneCount = oneCount + 1
-    true
-  }
-
-  def incrementZero(): Boolean = {
-    zeroCount = zeroCount + 1
-    true
-  }
-
-  def mostCommonBit(): Char = {
-    if (oneCount >= zeroCount) '1'
-    else if (oneCount < zeroCount) '0'
-    else '1'
-  }
-}
